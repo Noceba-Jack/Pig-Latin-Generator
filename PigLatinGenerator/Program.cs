052a1940ecab2964b4d638ca395d8092e9b9de27
Jack Nonceba <144227702+wackyslime@users.noreply.github.com>
2024-06-25 18:26:21 +0200

    Add the program code

---
+++ PigLatinGenerator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PigLatinGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //error handling
+            try
+            {
+                Console.WriteLine("----------Pig Latin Generator----------");
+            Console.Write("Enter your sentence: ");
+            string sUserSentence = Console.ReadLine();
+            string[] wordsToConvert = sUserSentence.ToLower().Split(' ');
+
+            for (int i = 0; i< wordsToConvert.Length;i++) 
+            {
+                //check if word starts with a vowel
+                if (wordsToConvert[i].StartsWith('a') || wordsToConvert[i].StartsWith('e') || wordsToConvert[i].StartsWith('i') || wordsToConvert[i].StartsWith('o') || wordsToConvert[i].StartsWith('u'))
+                {
+                    //add way to the end of the word
+                    wordsToConvert[i] = wordsToConvert[i] + "way";
+                }
+                else
+                {
+                    string sWord = wordsToConvert[i];
+                    char cFirstLetter = sWord[0];//get first letter of word
+                    string sTrimmmedWord = sWord.Remove(0, 1);//remove first letter from word
+                    wordsToConvert[i] = sTrimmmedWord + cFirstLetter + "ay";//add last letter and 'ay'
+                }
+            }
+
+            //display the pig latin sentence
+            Console.Write("Your sentence in pig latin: ");
+            foreach (string word in wordsToConvert)
+            {
+                Console.Write(word + ' ');
+            }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }//Try catch
+        }//Main()
+    }
+}
